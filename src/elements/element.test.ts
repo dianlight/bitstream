1594aac093dafb486b989e3544ab63ca33df5416
dianlight <lucio.tarantino@gmail.com>
2024-11-04 00:15:58 +0100

    Add trasformers test from write to read

--- src/elements/element.test.ts
+++ src/elements/element.test.ts
@@ -195,6 +195,23 @@ describe('BitstreamElement', it => {
             expect(a2.num1).to.equal(24);
             expect(a2.num2).to.equal(23.6);
         });
+        it('@Field() accept write and read transformer', async () => {
+            class A extends BitstreamElement {
+                @Field(16,{number: {format: 'unsigned'},transformers: {write:(v)=>v/2, read:(v)=>v*2}}) num1: number;
+                @Field(16,{number: {format: 'unsigned'},transformers: {write:(v)=>v*10, read:(v)=>v/10}}) num2: number;
+            }
+
+            let buf = Buffer.from('000E0401','hex');
+
+            let a = A.deserialize(buf);
+            expect(a.num1).to.equal(28);
+            expect(a.num2).to.equal(102.5);
+
+            buf = Buffer.from(a.serialize());
+        
+            expect(buf.readUInt16BE(0)).to.equal(14);
+            expect(buf.readUInt16BE(2)).to.equal(1025);
+        });
     });
 
     it('@Field() accepts single options when length is inferred', async () => {
